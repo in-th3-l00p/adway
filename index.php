347c20e5622210cd4dfff574b8b8c3002d914600
intheloop <indianucode0303@gmail.com>
2022-11-18 23:43:56 +0200

    homepage prototype

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AdWay - digital marketing</title>
+
+    <link rel="stylesheet" href="/adway/css/globals.css">
+    <link rel="stylesheet" href="/adway/css/index.css">
+    <link rel="stylesheet" href="/adway/bootstrap-5.2.2-dist/css/bootstrap.min.css">
+    <script defer src="/adway/bootstrap-5.2.2-dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <?php include "navbar/navbar.php"; ?> 
+    <section>
+        <video class="header-video" muted autoplay loop>
+            <source 
+                src="https://drive.google.com/u/3/uc?id=1cnX5t8pnwuirrZOmmpl1wfXy5NXklN-g&export=download&confirm=t&uuid=5f06ba49-138c-4dbc-8428-527e6a30ac28&at=AHV7M3cjvw2kyDkV6-N04YRnXwfY:1668807671066" 
+                type="video/mp4"
+            >
+        </video>
+        <div class="header-text">
+            <h1>AdWay</h1>
+            <h3>Digital marketing cum nu s-a mai vazut in universul asta</h3>
+        </div>
+    </section> 
+    <section class="introduction-section">
+        <div class="container">
+            <h2>Who are we?</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum, iste dolorum vel aperiam ipsum ratione velit illum, 
+                blanditiis nemo voluptate dolore nostrum provident facere. Nesciunt explicabo velit facere quam tenetur?
+            </p>
+        </div>
+    </section>
+</body>
+</html>
---
+++ navbar/navbar.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="/adway/css/navbar.css">
+<div class="nav">
+    <span class="brand">AdWay</span>
+    <nav>
+        <a href="/adway">Home</a>
+        <a href="/adway/about">About</a>
+        <a href="/adway/contact">Contact</a>
+    </nav>
+</div>
